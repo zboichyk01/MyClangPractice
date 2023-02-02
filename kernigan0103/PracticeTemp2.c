8b2a14d959038861ff94b60719ef1c461aa44594
barbara <zboichykb@appstate.edu>
2023-02-01 22:40:03 -0500

    Here is some Kernigan practice with C language 	new file:   kernigan0101hello/a.out 	new file:   kernigan0101hello/hello.c 	new file:   kernigan0101hello/hello.c.save 	new file:   kernigan0102/a.out 	new file:   kernigan0102/practice2.2.c 	new file:   kernigan0102/practice2.c 	new file:   kernigan0103/PracticeTemp2.c 	new file:   kernigan0103/a.out 	new file:   kernigan0103/practiceTemp.c

---
+++ kernigan0103/PracticeTemp2.c
@@ -0,0 +1,13 @@
+main () 
+{ float fahr, celsius; 
+	int lower, upper, step;
+	lower=0;
+	upper=300;
+	step=20;
+	fahr=lower;
+	while (fahr<= upper) 
+	{celsius = (5.0/9.0)*(fahr-32.0);
+		printf("%e.0f %6.1f\n", fahr, celsius);
+		fahr= fahr+step; }
+}
+
