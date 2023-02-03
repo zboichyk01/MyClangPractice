badfd6ec04fbba7385ac85aa36a303210fc92c82
barbara <zboichykb@appstate.edu>
2023-02-03 17:25:42 -0500

    New practice! created a title for temperature table 	new file:   a.out 	new file:   tempname3.c

---
+++ kernigan0104/tempname3.c
@@ -0,0 +1,12 @@
+main () 
+{printf("Farenheit Celsius Conversion Table\n");
+	int fahr, celsius;
+	int lower, upper, step;
+	lower = 0;
+	upper = 250; 
+	step = 20; 
+	fahr = lower;
+	while (fahr <= upper) { celsius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;} } 
+
