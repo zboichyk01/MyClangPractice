bda74565f15fe97f60f3d8944ee8c7299ee1bb08
barbara <zboichykb@appstate.edu>
2023-02-05 21:13:38 -0500

    changed some numbers around 	modified:   tempname3.c

--- kernigan0104/tempname3.c
+++ kernigan0104/tempname3.c
@@ -2,9 +2,9 @@ main ()
 {printf("Farenheit Celsius Conversion Table\n");
 	float fahr, celsius;
 	float lower, upper, step;
-	lower = 0;
-	upper = 250; 
-	step = 20; 
+	lower = 5;
+	upper = 350; 
+	step = 10; 
 	fahr = lower;
 	while (fahr <= upper) { celsius = (5.0/9.0)*(fahr-32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
